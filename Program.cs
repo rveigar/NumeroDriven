2de488fb485febcfd9d36c862fb81d4ddfd3f06d
Veiga <roberto.veiga@gmail.com>
2022-04-20 07:40:50 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace NumeroDriven
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            Console.WriteLine(NumeroDriven(1220703123));
+        }
+
+        public static string NumeroDriven(int numero)
+        {
+            for (int i = 0; i < 1000; i++)
+            {
+                int num = (int) Math.Pow(5,i) - 2;
+                Console.WriteLine($"valor de i= {i} num= {num}");
+                if (num == numero)
+                {
+                    return ("Numero Driven");
+                }
+            }
+            return ("Não é desse tipo");
+        }
+
+    }
+}
